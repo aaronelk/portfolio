08ff516c9f09cb4b1a1b13580a8eded5bbdd98db
Aaron Elkeslassy <aaron.elkeslassy@hotmail.com>
2021-04-02 01:02:56 -0400

    add gifs

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,15 +1,29 @@
 import "./projects.css";
 import { projects } from "../../content/projects";
+import { Project } from "./types";
+import { Fragment, useState } from "react";
+import { Modal } from "@material-ui/core";
 
 export default function Projects() {
+
+  const [modalState, setModalState] = useState({active: false, gifUrl: ''});
+
+
+  const runGifInModal = (project: Project) => {
+    if (project.gifUrl) {
+      setModalState({active: true, gifUrl: project.gifUrl})
+    }
+  }
+
   return (
+    <Fragment>
     <div className="projectsContainer">
       <div className="projectsInner">
         <div className="projectsTitle">My Projects</div>
         <div className="projectContent">
           {projects.map(project => {
             return (
-              <div className={`projectContainer ${project.size}`}>
+              <div className={`projectContainer ${project.size} ${project.gifUrl ? 'clickable' : ''}`} onClick={() => runGifInModal(project)}>
                 <img className="projectImage" src={project.image}/>
                 <div className="projectTitle">{project.name}</div>
               </div>
@@ -18,5 +32,15 @@ export default function Projects() {
         </div>
       </div>
     </div>
+    <Modal
+      open={modalState.active}
+      onClose={() => setModalState({active: false, gifUrl: ''})}
+      onClick={() => setModalState({active: false, gifUrl: ''})}
+      >
+        <div className="modalContainer">
+          <img  className="modalGif" src={modalState.gifUrl}/>
+        </div>
+      </Modal>
+    </Fragment>
   );
 }
--- src/content/projects.ts
+++ src/content/projects.ts
@@ -3,9 +3,11 @@ import { Project } from "../components/projects/types";
 import FloridaNaturalImg from "./images/florida-natural.svg";
 import JazztownImg from "./images/jazztown.svg";
 import TeatimeImg from "./images/teatime.svg";
+import FloridaNaturalGif from "./images/florida.gif";
+import JazzTownGif from "./images/jazztown.gif";
 
 export const projects: Project[] = [
-  { name: "Florida's Natural", image: FloridaNaturalImg, size: "full" },
-  { name: "Jazztown", image: JazztownImg, size: "half" },
+  { name: "Florida's Natural", image: FloridaNaturalImg, size: "full", gifUrl: FloridaNaturalGif },
+  { name: "Jazztown", image: JazztownImg, size: "half", gifUrl: JazzTownGif },
   { name: "TeaTime", image: TeatimeImg, size: "half" },
 ];
